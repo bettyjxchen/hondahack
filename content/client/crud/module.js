b6c5d21939165c9d01f78dce7097fcadeb1e6446
Edward Cadiz <cadiz.edward@gmail.com>
2017-11-27 14:03:09 -0800

    make a real branch ya dummy

--- content/client/crud/module.js
+++ content/client/crud/module.js
@@ -1,6 +1,32 @@
 /* global angular */
-(function () {
-    'use strict';
+(function() {
+    'use strict'
+    angular.module('client.crud')
+    angular.module('client.crud').config(RouteConfig)
 
-    angular.module('client.crud', [])
+    RouteConfig.$inject = ['$stateProvider']
+
+    function RouteConfig($stateProvider) {
+        $stateProvider
+            .state('site.right-nav.index', {
+                url: '/right-side',
+                abstract: true,
+                views: {
+                    'content': {
+                        templateUrl: '/content/client/crud/right-nav-bar/right-nav-bar-default.html',
+                        controller: ''
+                    }
+                }
+            })
+            .state('site.right-nav.index', {
+                url: '/nav-bar',
+                views: {
+                    'right-nav': {
+                        templateUrl: '/content/client/crud/right-nav-bar/right-nav-bar.html',
+                        controller: ''
+                    }
+                },
+            })
+    }
+
 })();
