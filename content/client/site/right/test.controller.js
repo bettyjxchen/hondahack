50c64aa8991504f14dce5a1efa3646f9c4254726
Kabindra <Kabindra@KABINDRA>
2017-11-28 03:26:20 -0800

    changess again

--- content/client/site/right/test.controller.js
+++ content/client/site/right/test.controller.js
@@ -9,7 +9,11 @@
     function SideNavControllerFunction($log) {
         var vm = this;
         vm.emergencyMsg = _emergencyMsg;
-        vm.sendMsg = _sendMsg;
+        vm.fuelMsg = _fuelMsg;
+        vm.lostMsg = _lostMsg;
+        vm.emergency = _emergency;
+        vm.callFriend = _callFriend;
+
         vm.airbags = {
             isActive: true,
             isDeployed: false,
@@ -17,18 +21,34 @@
             type: "FRONTAL"
         };
 
+        vm.fuel = {
+            amountRemaining: 53.2,
+            percentRemaining: 0.3,
+            range: 40.5
+          }
+
         function _emergencyMsg() {
             vm.airbags.isDeployed = true;
             console.log("airbags");
             var message = "Emergency message is sending to your close friends";
             if (vm.airbags.isDeployed == true) {
                 alert(message);
-                _sendMsg();
+                _fuelMsg();
             }
         }
 
-        function _sendMsg(){
+        function _fuelMsg(){
             vm.isActive = true;
         }
+
+        function _lostMsg(){
+            vm.isLost = true;
+        }
+        function _emergency(){
+            vm.emergencyActive = true;
+        }
+        function _callFriend(){
+            vm.frenCallActive = true;
+        }
     }
-})();
+})()
