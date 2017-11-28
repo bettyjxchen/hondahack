03394538bc910b392fa92909821bfc3689ab4ece
Kabindra <Kabindra@KABINDRA>
2017-11-28 00:41:08 -0800

    pulling not pushing

--- content/client/site/right/test.controller.js
+++ content/client/site/right/test.controller.js
@@ -1,35 +1,34 @@
-(function(){
-    "use strict"
+(function () {
+    "use strict";
+
     angular.module('client.site')
-            .controller("sideNavController", SideNavController);
+        .controller('sideNavController', SideNavControllerFunction)
 
-SideNavController.inject = ["$scope"];
-function SideNavController($scope){
+    SideNavControllerFunction.$inject = ['$log']
+
+    function SideNavControllerFunction($log) {
         var vm = this;
-        vm.$scope = $scope;
-        vm.$onInit = _onInit;
         vm.emergencyMsg = _emergencyMsg;
-
-        vm.airbags= {
+        vm.sendMsg = _sendMsg;
+        vm.airbags = {
             isActive: true,
             isDeployed: false,
             location: "FRONT_LEFT",
             type: "FRONTAL"
-            };
-        
-        
-        function _onInit(){
-            console.log("page is running ..");
-        }
+        };
 
-        function _emergencyMsg(){
+        function _emergencyMsg() {
             vm.airbags.isDeployed = true;
             console.log("airbags");
             var message = "Emergency message is sending to your close friends";
-            if(vm.airbags.isDeployed == true){
-                alert (message);
+            if (vm.airbags.isDeployed == true) {
+                alert(message);
+                _sendMsg();
             }
         }
 
-}
+        function _sendMsg(){
+            vm.isActive = true;
+        }
+    }
 })();
